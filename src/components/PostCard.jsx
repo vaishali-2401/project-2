4476c79d322e68c8b0dc06f94c75db7c335a852d
vaishali-2401 <vaishaligupta2401@gmail.com>
2025-07-11 19:25:13 +0530

    Update PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -9,7 +9,7 @@ function PostCard({$id, title, featuredImage}) {
         <div className='w-full bg-gray-100 rounded-xl p-4'>
             <div className='w-full justify-center mb-4'>
                 <div className='rounded-xl'> {title}
-                </div>>
+                </div>
 
             </div>
             <h2
