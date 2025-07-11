0369b10f8e4e7df3cce70f1d8fe560065beed895
vaishali-2401 <vaishaligupta2401@gmail.com>
2025-07-11 19:30:51 +0530

    Update PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,24 +1,14 @@
 import React from 'react'
-import appwriteService from "../appwrite/configure"
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-function PostCard({$id, title, featuredImage}) {
-    
+function PostCard({ $id, title }) {
   return (
     <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl p-4'>
-            <div className='w-full justify-center mb-4'>
-                <div className='rounded-xl'> {title}
-                </div>
-
-            </div>
-            <h2
-            className='text-xl font-bold'
-            >{title}</h2>
-        </div>
+      <div className="w-full bg-gray-100 rounded-xl p-4">
+        <h2 className="text-xl font-bold text-center">{title}</h2>
+      </div>
     </Link>
   )
 }
-
 
 export default PostCard
